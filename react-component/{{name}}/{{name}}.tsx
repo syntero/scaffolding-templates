814850959c1fbb468c6431c493fbebe8548220a9
urigat <uri.gat@syntero.io>
2024-12-24 14:16:40 +0200

    wip

---
+++ react-component/{{name}}/{{name}}.tsx
@@ -0,0 +1,11 @@
+import { {{name_with_underscores}} } from "./{{name_with_dashes}}.module.css"
+
+export type {{name}}Props = {};
+
+export const CompareFlow = ({ }: {{name}}Props) => {
+    return (
+        <div className={ {{name_with_underscores}} }>
+            {{name}}
+        </div>
+    )
+};
